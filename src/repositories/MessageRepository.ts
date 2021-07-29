cf7a63219b43581264c8c6c1089145a4f8ad237a
AlexSodreDev <alexandre.cortines@gmail.com>
2021-07-28 22:18:59 -0300

    refatoraçao do repository

--- src/repositories/MessageRepository.ts
+++ src/repositories/MessageRepository.ts
@@ -1,8 +1,19 @@
-import { EntityRepository, Repository } from "typeorm";
+import { EntityRepository, getCustomRepository, Repository } from "typeorm";
 
 import { Message } from "../models/MessageModel";
 
 @EntityRepository(Message)
-class MessageRepository extends Repository<Message> { }
+class MessageRepository extends Repository<Message> {
+  public async store(name: any) {
+    const messageRepository = getCustomRepository(MessageRepository);
 
-export { MessageRepository };
+    const message = messageRepository.create({
+      encrypted_name: name,
+    });
+
+    const messageCreated = await messageRepository.save(message);
+    return messageCreated;
+  }
+}
+
+export const messageRepository = new MessageRepository();
